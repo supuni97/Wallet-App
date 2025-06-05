8fc36fa646156cf066e0aec6a11e13202b0d4a05
supuni97 <sitharabandara1997@gmail.com>
2025-06-05 19:41:07 +0530

    added db url

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req, res) => {
+  res.send("It's working");
+});
+
+//console.log(process.env.PORT);
+
 app.listen(5001, () => {
-  console.log("Server is up and running on PORT:5001");
+  console.log("Server is up and running on PORT:", PORT);
 });
