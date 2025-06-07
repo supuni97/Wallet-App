a4bcb2107d612d6fc4366074b09a2feff1cde14b
supuni97 <sitharabandara1997@gmail.com>
2025-06-07 19:42:25 +0530

    added rate limiter

---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,18 @@
+import ratelimit from "../config/upstash";
+
+const ratelimiter = async (req, res, next) => {
+  try {
+    const { success } = await ratelimit.limit("my-rate-limit");
+
+    if (!success) {
+      res.status(429).json({ message: "Too many requests,try again later" });
+    }
+
+    next();
+  } catch (error) {
+    console.log("Rate limit error", error);
+    next(error);
+  }
+};
+
+export default ratelimiter;
