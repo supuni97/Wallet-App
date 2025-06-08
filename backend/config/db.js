1c1770bf62b3362bae068d56e43a0a4fa522958e
supuni97 <sitharabandara1997@gmail.com>
2025-06-08 12:03:42 +0530

    Added initDB function to db file

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,3 +3,21 @@ import "dotenv/config";
 
 //create a sql connection
 export const sql = neon(process.env.DATABASE_URL);
+
+export async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title VARCHAR(255) NOT NULL,
+      amount DECIMAL(10, 2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Database initializing fail", error);
+    process.exit(1);
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { sql } from "./config/db.js";
+import { initDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
 
@@ -17,29 +17,6 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5001;
-
-async function initDB() {
-  try {
-    await sql`CREATE TABLE IF NOT EXISTS transactions (
-      id SERIAL PRIMARY KEY,
-      user_id VARCHAR(255) NOT NULL,
-      title VARCHAR(255) NOT NULL,
-      amount DECIMAL(10, 2) NOT NULL,
-      category VARCHAR(255) NOT NULL,
-      created_at DATE NOT NULL DEFAULT CURRENT_DATE
-    )`;
-
-    console.log("Database initialized successfully");
-  } catch (error) {
-    console.log("Database initializing fail", error);
-    process.exit(1);
-  }
-}
-
-//test api
-app.get("/", (req, res) => {
-  res.send("It's working");
-});
 
 app.use("/api/transactions", transactionsRoute);
 
