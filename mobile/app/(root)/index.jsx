0d890d678ce877f3f4dd4e411e585700226d50dd
supuni97 <sitharabandara1997@gmail.com>
2025-06-16 12:56:20 +0530

    Added home page's header and balance card

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,11 +1,12 @@
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SignOutButton } from "../../components/SignOutButton";
 import { useTransitions } from "../../hooks/useTransactions";
 import { useEffect } from "react";
 import PageLoader from "../../components/PageLoader";
-
+import { styles } from "../../assets/styles/home.styles";
+import { Ionicons } from "@expo/vector-icons";
 export default function Page() {
   const { user } = useUser();
   const { transactions, summary, isLoading, loadData, deleteTransaction } =
@@ -23,19 +24,37 @@ export default function Page() {
   if (isLoading) return <PageLoader />;
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {/*left*/}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}{" "}
+              </Text>
+            </View>
+          </View>
+          {/*right*/}
+          <View style={styles.headerRight}>
+            <TouchableOpacity
+              style={styles.addButton}
+              onPress={() => router.push("/create")}
+            >
+              <Ionicons name="add" size={20} color="#fff" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+        <BalanceCard summary={summary} />
+      </View>
     </View>
   );
 }
--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,6 +1,7 @@
 import SafeScreen from "../components/SafeScreen";
 import { Slot } from "expo-router";
 import { ClerkProvider } from "@clerk/clerk-expo";
+
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 export default function RootLayout() {
   return (
